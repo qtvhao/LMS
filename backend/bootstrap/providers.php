7c7788b31f177b3e7e1412d62a2c7718da11051c
Your Name <you@example.com>
2024-11-11 16:41:18 +0100

    Add Device Access Control service provider and update Docker configuration for production and testing environments

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     Qtvhao\LearningModule\LearningModuleServiceProvider::class,
+    Qtvhao\DeviceAccessControl\Providers\DeviceAccessControlServiceProvider::class,
 ];
