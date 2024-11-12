cca2dfeb6abeae2bea3162aff4eb99e7b57144f4
Your Name <you@example.com>
2024-11-12 17:39:08 +0100

    Update dependencies and migration: bump Laravel and Monolog versions, rename device migration file, and enhance device_type definition with enums

--- backend/database/migrations/2024_11_12_162417_create_device_table.php
+++ backend/database/migrations/2024_11_12_162417_create_device_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Qtvhao\DeviceAccessControl\Core\Enums\DeviceEnums;
 
 return new class extends Migration {
     /**
@@ -15,8 +16,10 @@ return new class extends Migration {
         Schema::create('devices', function (Blueprint $table) {
             $table->id();
             $table->string('device_id')->unique(); // Unique identifier for each device
-            $table->enum('device_type', ['Web', 'Tablet', 'Mobile']);
+            $table->enum('device_type', [DeviceEnums::DEVICE_TYPE_WEB, DeviceEnums::DEVICE_TYPE_TABLET, DeviceEnums::DEVICE_TYPE_MOBILE]);
             $table->timestamps();
+
+            $table->integer('user_id')->unsigned();
         });
     }
 
