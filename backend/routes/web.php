106a32dedd5fac29713ea04bb982b08543bfc054
Your Name <you@example.com>
2024-11-14 07:35:36 +0100

    Add route for device management page with basic response

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/device-management', function () {
+    $response = new \Illuminate\Http\Response();
+    return $response->setContent('Device Management Page');
+});
 
 Route::view('/register', 'auth.register');
 Route::view('/login', 'auth.login');
